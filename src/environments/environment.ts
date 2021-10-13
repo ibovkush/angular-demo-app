037c02fec87ee6f7f499b64b06454fef64295749
Ivan Bovkush <ivan.bovkush@gmail.com>
2021-10-13 22:54:09 +0300

    feat: add zone error module in debug mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import 'zone.js/plugins/zone-error';
+
 
 export const environment = {
-  production: false
+  production: false,
 };
 
 /*
@@ -13,4 +15,3 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
